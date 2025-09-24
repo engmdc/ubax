fdbdb4d01db2df0a2c404a2426f27344c2207f0f
engmdc <90640129+engmdc@users.noreply.github.com>
2025-09-24 11:01:26 +0300

    Update __manifest__.py
    CLOUD CLUSTER INIT

--- idil/__manifest__.py
+++ idil/__manifest__.py
@@ -8,7 +8,7 @@
     "depends": ["mail", "point_of_sale", "web"],
     "application": True,
     "sequence": -100,
-    "author": "again",
+    "author": "MDC",
     "assets": {
         "web.assets_common": [
             # 'idil/static/src/scss/primary_variables.scss',
